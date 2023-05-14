adf66c95e3c1026a6873395dbdb611999ac2069f
Rabiizubair <124505715+Rabiizubair@users.noreply.github.com>
2023-05-14 19:43:46 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,29 @@
+ //The most basic types in TypeScript correspond to the seven basic kinds of primitives types in JavaScript:
+
+//string type
+let message = "Hello World";        //Infering Types, 
+console.log(message);               //take your cursor on the variable name
+
+//number type
+let number = 2;
+console.log(number);
+
+//boolean type
+let isMarried = true;
+console.log(isMarried);
+
+//undefined type
+let biscuit;
+console.log(biscuit);
+
+//null type
+let chocolate = null;
+console.log(chocolate);
+
+//bigint type
+let bigint = 5n;
+console.log(bigint);
+
+//symbol type
+let sym = Symbol("hi");
+console.log(sym);
